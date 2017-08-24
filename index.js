3537c83902e8dbb5134c42c70ed22982eb039699
lin-N <iroi_1982@163.com>
2017-08-24 17:25:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const os           = require('os');
 const mysql        = require('mysql');
 
 var connection = mysql.createConnection({
-    host     : 'rm-bp18525c38y858eyd.mysql.rds.aliyuncs.com',
-    user     : 'pubg',
-    password : 'LOYO_pubg',
-    database : 'pubg'
+    host     : 'localhost',
+    user     : 'p',
+    password : 'p',
+    database : 'p'
 });
 connection.connect();
 var sql = 'SELECT * FROM `key` WHERE `name` = "' + os.hostname() + '";';
